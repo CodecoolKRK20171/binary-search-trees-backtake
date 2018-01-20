2c50cc35c0d6e21078beeb12e85ddb2aa22a95af
backtake <stasiusoltysik@gmail.com>
2018-01-20 20:55:11 +0100

    Implement my hash map

---
+++ src/main/java/com/codecool/hashmap/KeyValue.java
@@ -0,0 +1,24 @@
+package com.codecool.hashmap;
+
+public class KeyValue {
+    private String key;
+    private Integer value;
+
+    public KeyValue(String key, Integer value) {
+        if (key==null) throw new IllegalArgumentException("Key in MyHashMap cannot be a null!");
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+}
